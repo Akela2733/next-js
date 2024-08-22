cb7a393ecbf03c27fafd9d7eac6286e8fe7f1478
Akela2733 <sakuniakela273@gmal.com>
2024-08-22 09:03:13 +0530

    feat: Implemented nav bar and added authentication functionality

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import DesktopLogo from "../../public/airbnb-desktop.png";
+import MobileLogo from "../../public/airbnb-mobile.webp";
+import UserNav from "./UserNav";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full border-b">
+      <div className="flex items-center justify-between container mx-auto px-5 lg:px-10 py-5 ">
+        <Link href="/">
+          <Image src={DesktopLogo} alt="DesktopLogo" className="w-32 hidden lg:block" />
+          <Image src={MobileLogo} alt="MobileLogo" className="w-12 block lg:hidden" />
+        </Link>
+
+        <div className="rounded-full border px-5 py-2">
+          <h1>Hello from the search</h1>
+        </div>
+        <UserNav />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
